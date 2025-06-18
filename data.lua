680beed9b1c6b309a8ce156f45a58df8e1f8a347
Funtime60 <Funtime60@users.noreply.github.com>
2025-06-17 21:51:56 -0600

    Add reverse mode to shortcut

--- data.lua
+++ data.lua
@@ -13,10 +13,18 @@ local select_mode = {
 	cursor_box_type = "pair",
 	entity_type_filters = {"electric-pole", "lamp"}
 }
+local reverse_mode = {
+	mode = {"any-entity"},
+	border_color = {r = 1, g = 1, b = 0},
+	cursor_box_type = "pair",
+	entity_type_filters = {"lamp"}
+}
 local selector =	 {
 	type = "selection-tool",
 	select = select_mode,
 	alt_select = select_mode,
+	reverse_select = reverse_mode,
+	alt_reverse_select = reverse_mode,
 	name = "auto-lamp-tool",
 	icon = data.raw["item"]["small-lamp"].icon,
 	flags = {"only-in-cursor"},
